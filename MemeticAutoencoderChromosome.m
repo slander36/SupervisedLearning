9feeb897e6162d71902f465d9da3ebdf245a6dab
Alex Paino <atpaino@gmail.com>
2013-11-15 14:11:56 -0800

    MemeticAutoencoderchromosome done, untested

--- MemeticAutoencoderChromosome.m
+++ MemeticAutoencoderChromosome.m
@@ -6,6 +6,7 @@ classdef MemeticAutoencoderChromosome < handle
         
         autoEncoder % Holds weights for this autoencoder
         modified = true;   % Determines if the fitness need to be updated
+        hiddenLayerSize % Hidden layer size
     end
     
     methods
@@ -16,20 +17,31 @@ classdef MemeticAutoencoderChromosome < handle
             elseif nargin < 2
                 % Initializes chromosome randomly
                 chromo.autoEncoder = randn(params.hiddenLayerSize, 1);
+                chromo.hiddenLayerSize = params.hiddenLayerSize;
             else
                 % Make a deep copy of the given chromosome
                 chromo.autoEncoder = chromoToClone.autoEncoder;
+                chromo.hiddenLayerSize = chromoToClone.hiddenLayerSize;
             end
         end
    
         function mutate(self, pMutation)
             % Mutates this chromosome's tree by recursively traversing it
-            % TODO
+            if pMutation > rand()
+                self.autoEncoder = self.autoEncoder .*...
+                                   exp(randn(self.hiddenLayerSize, 1));
+                self.modified = true;
+            end
         end
         
         function offspring = crossover(parent1, parent2)
             % Performs crossover between two given parents
-            % TODO
+            offspring = MemeticAutoencoderChromosome([], parent1)
+            blend = rand(self.hiddenLayerSize, 1);
+            offspring.autoEncoder = offspring.autoEncoder .* blend +...
+                                    parent2.autoEncoder .*...
+                                    (repmat(1, parent1.hiddenLayerSize, 1) - blend);
+            offspring.modified = true;
         end
         
     end
